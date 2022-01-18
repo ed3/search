6873e2f2637ccd66fd55dcc735c9bb49237bf832
Ed <ed3@users.noreply.github.com>
2022-01-18 08:02:29 +0200

    cakephp 3.10

--- src/Model/Behavior/SearchableBehavior.php
+++ src/Model/Behavior/SearchableBehavior.php
@@ -232,7 +232,7 @@ class SearchableBehavior extends Behavior {
 		$cond = [];
 		foreach ($fieldNames as $fieldName) {
 			if (strpos($fieldName, '.') === false) {
-				$fieldName = $this->_table->alias() . '.' . $fieldName;
+				$fieldName = $this->_table->getAlias() . '.' . $fieldName;
 			}
 
 			if ($field['before'] === true) {
@@ -317,7 +317,7 @@ class SearchableBehavior extends Behavior {
 		$cond = [];
 		foreach ($fieldNames as $fieldName) {
 			if (strpos($fieldName, '.') === false) {
-				$fieldName = $this->_table->alias() . '.' . $fieldName;
+				$fieldName = $this->_table->getAlias() . '.' . $fieldName;
 			}
 			if (is_array($fieldValue) && empty($fieldValue)) {
 				continue;
@@ -329,7 +329,7 @@ class SearchableBehavior extends Behavior {
 			if (is_array($fieldValue) || !is_array($fieldValue) && (string)$fieldValue !== '') {
 				$cond[$fieldName] = $fieldValue;
 			} elseif (isset($data[$field['name']]) && !empty($field['allowEmpty'])) {
-				$schema = $this->_table->schema()->column($field['name']);
+				$schema = $this->_table->getSchema()->getColumn($field['name']);
 				if (isset($schema) && ($schema['default'] !== null || !empty($schema['null']))) {
 					$cond[$fieldName] = $schema['default'];
 				} elseif (!empty($fieldValue)) {
